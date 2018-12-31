946027c55d5cb8135f49447c757a695510d3c99b
Tim Brugman <tim@timbrugman.com>
2018-12-31 11:07:28 +0100

    Cleaning up.

--- js/theme.js
+++ js/theme.js
@@ -2,18 +2,3 @@ document.querySelector( '#visitor-ip' ).select();
 
 new ClipboardJS( '.js-clipboard' );
 
-// var clipboard = new ClipboardJS( '.js-clipboard' );
-
-// clipboard.on('success', function(e) {
-//     console.log(e);
-// });
-
-// clipboard.on('error', function(e) {
-//     console.log(e);
-// });
-
-// $( '#visitor-ip' ).focus().select();
-
-// (function($) {
-//     $( '#visitor-ip' ).focus().select();
-// })( jQuery );
