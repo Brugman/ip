dd16a39ed593338a6fa91bf25e6e6de929034b94
Tim Brugman <tim@timbrugman.com>
2019-07-25 15:06:02 +0200

    Changed favicon.

--- public_html/index.php
+++ public_html/index.php
@@ -11,8 +11,10 @@
     <title>Your IP</title>
 
     <!-- favicon -->
-    <link rel="apple-touch-icon" href="./favicon.png">
-    <meta name="msapplication-TileImage" content="./favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+    <link rel="manifest" href="./site.webmanifest">
 
     <!-- IP CSS -->
     <link rel="stylesheet" href="./assets/css/ip-theme.min.css" />
